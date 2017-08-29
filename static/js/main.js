93940a7ef9ddc5b739223ea02d71ed4a7b596a41
ChrisWilliamson123 <cjwilliamson109@gmail.com>
2017-08-29 18:16:14 +0300

    Spacebar starts movement of the ball

--- static/js/main.js
+++ static/js/main.js
@@ -1,3 +1,35 @@
 $(document).ready(function() {
+    var playerOneY;
+    var ballSpeed = 5;
+    var ballAngle = 90;
+    var ballMovementInterval;
+
+    var distanceBetween = $('#p-two').offset().left - ($('#p-one').offset().left+15)
     
+    $(document).mousemove(function(e) {
+        playerOneY = e.pageY;
+        updatePlayerOnePosition();
+    })
+
+    $(document).keypress(function(e) {
+        var keyID = e.which;
+        if (keyID == 32) {
+            startBallMovement();
+        }
+    });
+
+    function startBallMovement() {
+        var currentLeft = $('#ball').offset().left;
+        ballMovementInterval = setInterval(function() {
+            var newLeft = currentLeft + distanceBetween/(60*ballSpeed);
+            $('#ball').css('left', newLeft);
+            currentLeft = newLeft;
+        }, 1000/60);
+    }
+
+    function updatePlayerOnePosition() {
+        if (playerOneY > 20) {
+            $('#p-one').css('top', playerOneY-20);
+        }
+    }
 })
